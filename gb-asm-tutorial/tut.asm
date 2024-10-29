b5f9e95924f37093d6ff6507920ccc1c097f2c75
Jason Knoll <jasonknoll98@aol.com>
2024-10-29 19:41:35 -0400

    Added tutorial

--- gb-asm-tutorial/tut.asm
+++ gb-asm-tutorial/tut.asm
@@ -8,7 +8,7 @@ SECTION "Header", ROM0[$100]
     ds $150 - @, 0
 
 EntryPoint:
-
+    ; empty and moves on to WaitVBlank
 
 WaitVBlank:
     ld a, [rLY] ; load into a, the value at rLY
@@ -46,16 +46,76 @@ CopyTilemap:
     or a, c
     jp nz, CopyTilemap
 
-    ; Turn the LCD back on
-    ld a, LCDCF_ON | LCDCF_BGON
-    ld [rLCDC], a ; will be 1 to turn on
+    ; Copy the paddle tile
+    ld de, Paddle
+    ld hl, $8000
+    ld bc, PaddleEnd - Paddle
+
+CopyPaddle:
+    ld a, [de]
+    ld [hli], a
+    inc de
+    dec bc
+    ld a, b
+    or a, c
+    jp nz, CopyPaddle
+
+    ld a, 0
+    ld b, 160
+    ld hl, _OAMRAM
+
+ClearOam:
+    ld [hli], a
+    dec b
+    jp nz, ClearOam
+
+    ld hl, _OAMRAM
+    ld a, 128 + 16
+    ld [hli], a
+    ld a, 16 + 8
+    ld [hli], a
+    ld a, 0
+    ld [hli], a
+    ld [hli], a
+
+    ; Turn the LCD on
+    ld a, LCDCF_ON | LCDCF_BGON | LCDCF_OBJON
+    ld [rLCDC], a
 
     ; During the first (blank) frame, initialize display registers
     ld a, %11100100
     ld [rBGP], a
+    ld a, %11100100
+    ld [rOBP0], a
 
-Done:
-    jp Done
+    ; Initialize global variables
+    ld a, 0
+    ld [wFrameCounter], a
+
+Main:
+    ld a, [rLY]
+    cp 144
+    jp nc, Main
+WaitVBlank2:
+    ld a, [rLY]
+    cp 144
+    jp c, WaitVBlank2
+
+    ld a, [wFrameCounter]
+    inc a
+    ld [wFrameCounter], a
+    cp a, 15 ; Every 15 frames (a quarter of a second), run the following code
+    jp nz, Main
+
+    ; Reset the frame counter back to 0
+    ld a, 0
+    ld [wFrameCounter], a
+
+    ; Move the paddle one pixel to the right.
+    ld a, [_OAMRAM + 1]
+    inc a
+    ld [_OAMRAM + 1], a
+    jp Main
 
 Tiles:
     dw `33333333
@@ -162,3 +222,17 @@ db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $1
 db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $16, $17, $18, $19, $03, 0,0,0,0,0,0,0,0,0,0,0,0
 db $04, $09, $09, $09, $09, $09, $09, $09, $09, $09, $09, $09, $09, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
 TilemapEnd:
+
+Paddle:
+    dw `13333331
+    dw `30000003
+    dw `13333331
+    dw `00000000
+    dw `00000000
+    dw `00000000
+    dw `00000000
+    dw `00000000
+PaddleEnd:
+
+SECTION "Counter", WRAM0
+wFrameCounter: db
